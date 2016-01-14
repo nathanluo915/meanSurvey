ce3ea693b0f9fddd959bda72dd5140efe5a07180
Nathan Luo <naipton@gmail.com>
2016-01-13 20:07:01 -0500

    Change survey object structure to include internal indexing

--- routes/surveys.js
+++ routes/surveys.js
@@ -1,29 +1,15 @@
-// var db = require('../db/MongoSetup');
 var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next){
-  var survey = {
-    name: req.body.name,
-    description: req.body.description,
-    createdAt: new Date(),
-    questions: []
-  };
+  var survey = req.body
 
-  for (var qIndex in req.body.questions) {
-    var question = {
-      content: req.body.questions[qIndex].content,
-      answers: []
-    };
+  for (var qIndex in survey.questions) {
     for (var ansIndex in req.body.questions[qIndex].answers) {
-      var answer ={
-        content: req.body.questions[qIndex].answers[ansIndex]
-      }
-      question.answers.push(answer);
+      survey.questions[qIndex].answers[ansIndex].count = 0;
     }
-    survey.questions.push(question);
   }
-
+  console.log(survey);
   var collection = req.db.get('surveys');
   collection.insert(survey, function(err, result){
     res.json({survey: result});
@@ -39,10 +25,21 @@ router.get('/', function(req, res, next) {
 
 router.get('/:id', function(req, res, next) {
   var collection = req.db.get('surveys');
-  console.log(req);
   collection.findOne({_id: req.params.id}, function(err, survey) {
     res.json({survey: survey});
   })
-})
+});
+
+router.put('/:id', function(req, res, next) {
+  var collection = req.db.get('surveys');
+  collection.findOne({_id: req.body.id}, function(err, survey) {
+    for (var qIndex in req.body.responses){
+    // survey.questions[req.body.
+
+    }
+  })
+  console.log(req.body);
+
+});
 
 module.exports = router;
