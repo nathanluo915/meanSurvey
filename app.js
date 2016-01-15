69436d747e444de097a5b9c16f7e6fdb5afd59cf
Nathan Luo <naipton@gmail.com>
2016-01-14 22:46:06 -0500

    Removed Monk and Mongolia for MongoDB driver wrapper due to limitation of bulkwrite.
    MongoDB native node driver provides a function bulkwrite for ordered or non-ordered query (in my case is insertion), but Monk and Mongolia don't support bulkwrite. One problem of using native driver is to pass along an open-connection. MongoClient.connect or connection.open are async function, which takes callback to return a connection object, the server script need to be run after this connection is established to pass along the connection for routing.

--- app.js
+++ app.js
@@ -25,51 +25,64 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB setup
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/myapp');
-
+/*
+var connection = require('./db/db.js');
+var db = connection.db;
 app.use(function(req,res,next){
   req.db = db;
   next();
 });
+*/
+
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/myapp';
+MongoClient.connect(url, function(err, db) {
+  console.log("Connected to server");
+  app.use(function(req,res,next){
+    req.db = db;
+    next();
+  });
+
+  app.use('/', routes);
+  app.use('/users', users);
+  app.use('/surveys', surveys);
 
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/surveys', surveys);
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
+  // error handlers
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
+
+
+
 
 
 module.exports = app;
