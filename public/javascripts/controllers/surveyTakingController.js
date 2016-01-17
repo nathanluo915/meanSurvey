a814bcea619c519681f19d36358239c2fae85853
Nathan Luo <naipton@gmail.com>
2016-01-16 20:26:15 -0500

    Change from user and session for validation to cookie for anonymous survey creation and voting memory. Use cookieParser as middleware to read and add cookie to every client visit. Cookie is first set when user visit the site, originally a blank field. Cookie modification happens when user submit a vote for a survey, where the survey's id will be stored in the cookie. Every time user trying to reach the voting page of a survey will first go through a cookie check to see if the user has voted on the survey before. If so the user will be redirected to the result page. This check happens in surveyTakingController of angular.

--- public/javascripts/controllers/surveyTakingController.js
+++ public/javascripts/controllers/surveyTakingController.js
@@ -1,8 +1,19 @@
-app.controller('surveyTakingController', ['$scope', 'SurveyService', '$routeParams', '$location', function($scope, SurveyService, $routeParams, $location){
+app.controller('surveyTakingController', ['$scope', 'SurveyService', '$routeParams', '$location', '$cookies', function($scope, SurveyService, $routeParams, $location, $cookies){
   $scope.id = $routeParams.id;
   $scope.formData = {};
   SurveyService.getSurvey($scope.id).then(function(survey){
-    $scope.survey = survey;
+    var votedSurveys = $cookies.get('voted').split('&');
+    var voted = false;
+    for (var i = 0; i < votedSurveys.length; i++) {
+      if (survey._id === votedSurveys[i]) {
+        voted = true;
+      }
+    }
+    if (voted) {
+      $location.path('/surveys/' + survey._id + '/result');
+    } else {
+      $scope.survey = survey;
+    }
   });
 
   $scope.dummy = function(){
--- public/javascripts/services/SurveyService.js
+++ public/javascripts/services/SurveyService.js
@@ -2,8 +2,21 @@ app.factory('SurveyService', ['$http', '$q', function($http, $q) {
   return {
     addSurvey: function(survey) {
       var deferred = $q.defer();
-      $http.post('/surveys', survey).then(function(data){
-        deferred.resolve(data.data.survey);
+      surveyCop = survey;
+      delete surveyCop.questions.length;
+      console.log(surveyCop);
+      var questions = [];
+      for (var q in surveyCop.questions) {
+        var answers = [];
+        for (var a in surveyCop.questions[q].answers){
+          answers.push(surveyCop.questions[q].answers[a]);
+        }
+        surveyCop.questions[q].answers = answers;
+        questions.push(surveyCop.questions[q]);
+      }
+      surveyCop.questions = questions;
+      $http.post('/surveys', surveyCop).then(function(data){
+        deferred.resolve(data.data);
       })
       return deferred.promise;
     },
