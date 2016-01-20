c56e7f25061fc2fff39b0f23bf665486cb0c87a5
Nathan Luo <naipton@gmail.com>
2016-01-19 22:52:45 -0500

    Fixed a bug after removing a required input field the requirement is not removed, making the form not submittable.

--- public/javascripts/controllers/createController.js
+++ public/javascripts/controllers/createController.js
@@ -1,6 +1,7 @@
 app.controller('createController', ['$scope', '$compile', 'SurveyService', '$location', function($scope, $compile, SurveyService, $location){
   $scope.counter = 0;
-  $scope.survey = {questions: {length: 0}}
+  $scope.survey = {};
+  $scope.survey.questions = {length: 0};
   $scope.confirmation = [];
   $scope.createSurvey = function(survey, newSurveyForm){
     if (newSurveyForm.$valid){
@@ -17,6 +18,8 @@ app.controller('createController', ['$scope', '$compile', 'SurveyService', '$loc
       $scope.survey.questions.length += 1;
       $scope.confirmation[counter] = false;
       $scope.counter += 1;
+      $scope.survey.questions[counter] = {content: ""};
+      $scope.survey.questions[counter].answers = {1: {content: ""}, 2: {content: ""}};
     } else {
       alert("Reached max amount of questions");
     }
@@ -28,6 +31,7 @@ app.controller('createController', ['$scope', '$compile', 'SurveyService', '$loc
     if (nextIndex <= 10) {
       var addEle = $compile("<answer q-index='"+counter+"' ans-index='"+nextIndex+"' />")($scope);
       targetEle.append(addEle);
+      $scope.survey.questions[counter].answers[nextIndex] = {content: ""};
     } else {
       alert("Reached max amount of answers");
     }
@@ -55,11 +59,11 @@ app.controller('createController', ['$scope', '$compile', 'SurveyService', '$loc
   $scope.removeAnswer = function(qCounter) {
     var index = angular.element('#question-'+qCounter+'>.answer-block').children().length;
     var element = angular.element('#question-'+qCounter+'>.answer-block').children()[index-1];
-    if (element && index > 1){
+    if (element && index > 2){
       element.remove();
-    }
-    if($scope.survey && $scope.survey.questions[qCounter] && $scope.survey.questions[qCounter].answers[index]) {
-      delete $scope.survey.questions[qCounter].answers[index];
+      if($scope.survey && $scope.survey.questions[qCounter] && $scope.survey.questions[qCounter].answers[index]) {
+        delete $scope.survey.questions[qCounter].answers[index];
+      }
     }
   }
 
